f2fd70979d139435237fa2ea9abdcd1523551479
vivek vanga <vsrivivek9@gmail.com>
2014-01-19 22:22:18 +0530

    indentation

--- GraphAdjList.hpp
+++ GraphAdjList.hpp
@@ -12,77 +12,74 @@ using namespace std;
 
 template<class Elem>
 class GraphAdjList:public GraphAdt<Elem> {
-	private:
-	    struct node {
-     	int vertex;
+    private:
+        struct node {
+            int vertex;
 	    Elem e;
-	    };
-	    vector<list<node> > adjList;
-		Elem defElem;
+	};
+	vector<list<node> > adjList;
+	Elem defElem;
     public:
-	    GraphAdjList(int size,Elem defElem):adjList(size, list<node>()) {
-		    this->defElem= defElem;
-		}
-		GraphAdjList (const GraphAdjList<Elem> &g):adjList(g.adjList) {
-		    this->defElem= g.defElem;
-		}
-		~GraphAdjList() {}
-	    bool isConnected(int a, int b, char C) {
-		    vector<int> explored(adjList.size(),0);
-			stack<int> q;
-			typename list<node>::iterator itr;
-			q.push(a);
-			while(q.size()!=0) {
-			    int temp=q.top();
-				explored[temp]=1;
-				q.pop();
-				if(explored[b]==1) return true;
-				
-				for(itr=adjList[temp].begin();itr!= adjList[temp].end();itr++) {
-				    
-			        if((*itr).e==C && explored[(*itr).vertex]==0) 
-				        q.push((*itr).vertex);
-		        }
-			}
-			if( explored[b]==0) return false;
-			else return true;		
-		}		
-		void printGraph() {
-		    int vertex=0;
-		    typename list<node>::const_iterator itrIn;
-		    typename vector<list<node> >::const_iterator itrOut;
+	GraphAdjList(int size,Elem defElem):adjList(size, list<node>()) {
+	    this->defElem= defElem;
+	}
+	GraphAdjList (const GraphAdjList<Elem> &g):adjList(g.adjList) {
+	    this->defElem= g.defElem;
+	}
+	~GraphAdjList() {}
+	bool isConnected(int a, int b, char C) {
+	    vector<int> explored(adjList.size(),0);
+	    stack<int> q;
+	    typename list<node>::iterator itr;
+	    q.push(a);
+	    while(q.size()!=0) {
+	        int temp=q.top();
+	 	explored[temp]=1;
+		q.pop();
+		if(explored[b]==1) return true;
+		for(itr=adjList[temp].begin();itr!= adjList[temp].end();itr++) {
+		    if((*itr).e==C && explored[(*itr).vertex]==0) 
+		    q.push((*itr).vertex);
+	        }
+	    }
+	if( explored[b]==0) return false;
+	else return true;		
+	}		
+	void printGraph() {
+	    int vertex=0;
+	    typename list<node>::const_iterator itrIn;
+	    typename vector<list<node> >::const_iterator itrOut;
             for(itrOut= adjList.begin();itrOut!=adjList.end();itrOut++) {
                 cout<<vertex++<<"   ";
-				for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
+		for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
                     cout<<(*itrIn).vertex<<"->"; 				
                 }
-				cout<<"NULL"<<endl;
-				cout<<"val ";
-				for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
+		cout<<"NULL"<<endl;
+         	cout<<"val ";
+	        for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
                     cout<<(*itrIn).e<<"->"; 				
                 }
-				cout<<"NULL"<<endl;
+		cout<<"NULL"<<endl;
             }
         }			
-		void setEdge(int  from, int to, Elem e ) {
-		    node n1,n2;
-			n2.e=n1.e=e;
-			n1.vertex=to;
-			adjList[from].push_back(n1);
-			n2.vertex=from;
-			adjList[to].push_back(n2);
-		} 
-		Elem getEdge(int from, int to) {
-		    typename list<node>::const_iterator itr;
-			
-			for( itr= adjList[from].begin();itr!=adjList[from].end();itr++) {
-			    if( (*itr).vertex == to) return (*itr).e;
-			}
-			return defElem;
-		}
+	void setEdge(int  from, int to, Elem e ) {
+	    node n1,n2;
+	    n2.e=n1.e=e;
+	    n1.vertex=to;
+	    adjList[from].push_back(n1);
+	    n2.vertex=from;
+	    adjList[to].push_back(n2);
+	} 
+	Elem getEdge(int from, int to) {
+	    typename list<node>::const_iterator itr;
+	    for( itr= adjList[from].begin();itr!=adjList[from].end();itr++) {
+	    if( (*itr).vertex == to) return (*itr).e;
+	    }
+	    return defElem;
+	}
         int size() {
-		    return adjList.size();
-		}
+	    return adjList.size();
+	}
 };
 
 #endif
--- GraphAdjListCompMoves.hpp
+++ GraphAdjListCompMoves.hpp
@@ -14,78 +14,76 @@ using namespace std;
 
 template<class Elem>
 class GraphAdjListCompMoves {
-	private:
-	    struct node {
-     	int vertex;
+    private:
+        struct node {
+     	    int vertex;
 	    Elem e;
-	    };
-	    vector<list<node> > adjListCompMoves;
-		Elem defElem;
+	};
+	vector<list<node> > adjListCompMoves;
+	Elem defElem;
     public:
-	    GraphAdjListCompMoves(int size,Elem defElem):adjListCompMoves(size, list<node>()) {
-		    this->defElem= defElem;
-		}
-		GraphAdjListCompMoves (const GraphAdjListCompMoves<Elem> &g):adjListCompMoves(g.adjListCompMoves) {
-		    this->defElem= g.defElem;
-		}
-		~GraphAdjListCompMoves() {}
-	    bool isConnected(int &a, int& b) {
+	GraphAdjListCompMoves(int size,Elem defElem):adjListCompMoves(size, list<node>()) {
+	    this->defElem= defElem;
+	}
+	GraphAdjListCompMoves (const GraphAdjListCompMoves<Elem> &g):adjListCompMoves(g.adjListCompMoves) {
+	    this->defElem= g.defElem;
+	}
+	~GraphAdjListCompMoves() {}
+        bool isConnected(int &a, int& b) {
 		    //cout<<"here";	
-		    int size=adjListCompMoves.size();
-			char explored[size];
-			memset(explored,0,size* sizeof(char));
-			int stck[adjListCompMoves.size()];
-			int stckptr=0;
-			typename list<node>::iterator itr;
-			stck[stckptr++]=a;
-			
-			while(stckptr!=0) {
-			    int temp=stck[--stckptr];
-				explored[temp]=1;
-				if(explored[b]==1) return true;
-				for(itr=adjListCompMoves[temp].begin();itr!= adjListCompMoves[temp].end();itr++) {
-				    if(explored[(*itr).vertex]==0) 
-				        stck[stckptr++]=((*itr).vertex);
-		        }
-			}
-			 return false;		
-		}		
-		void printGraph() {
-		    int vertex=0;
-		    typename list<node>::const_iterator itrIn;
-		    typename vector<list<node> >::const_iterator itrOut;
+            int size=adjListCompMoves.size();
+	    char explored[size];
+	    memset(explored,0,size* sizeof(char));
+	    int stck[adjListCompMoves.size()];
+	    int stckptr=0;
+	    typename list<node>::iterator itr;
+	    stck[stckptr++]=a;
+	    while(stckptr!=0) {
+	        int temp=stck[--stckptr];
+                explored[temp]=1;
+	        if(explored[b]==1) return true;
+	        for(itr=adjListCompMoves[temp].begin();itr!= adjListCompMoves[temp].end();itr++) {
+	            if(explored[(*itr).vertex]==0) 
+	            stck[stckptr++]=((*itr).vertex);
+	        }
+	    }
+	return false;		
+        }		
+	void printGraph() {
+	    int vertex=0;
+	    typename list<node>::const_iterator itrIn;
+	    typename vector<list<node> >::const_iterator itrOut;
             for(itrOut= adjListCompMoves.begin();itrOut!=adjListCompMoves.end();itrOut++) {
-                cout<<vertex++<<"   ";
-				for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
+                 cout<<vertex++<<"   ";
+	        for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
                     cout<<(*itrIn).vertex<<"->"; 				
                 }
-				cout<<"NULL"<<endl;
-				cout<<"val ";
-				for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
+	        cout<<"NULL"<<endl;
+	        cout<<"val ";
+	        for(itrIn=(*itrOut).begin();itrIn!=(*itrOut).end();itrIn++) {
                     cout<<(*itrIn).e<<"->"; 				
                 }
-				cout<<"NULL"<<endl;
+	        cout<<"NULL"<<endl;
             }
         }			
-		void setEdge(int from, int to, Elem e ) {
-		    node n1,n2;
-			n2.e=n1.e=e;
-			n1.vertex=to;
-			adjListCompMoves[from].push_back(n1);
-			n2.vertex=from;
-			adjListCompMoves[to].push_back(n2);
-		} 
-		Elem getEdge(int from, int to) {
-		    typename list<node>::const_iterator itr;
-			
-			for( itr= adjListCompMoves[from].begin();itr!=adjListCompMoves[from].end();itr++) {
-			    if( (*itr).vertex == to) return (*itr).e;
-			}
-			return defElem;
-		}
+	void setEdge(int from, int to, Elem e ) {
+	    node n1,n2;
+	    n2.e=n1.e=e;
+	    n1.vertex=to;
+	    adjListCompMoves[from].push_back(n1);
+	    n2.vertex=from;
+	    adjListCompMoves[to].push_back(n2);
+	} 
+	Elem getEdge(int from, int to) {
+	    typename list<node>::const_iterator itr;
+	    for( itr= adjListCompMoves[from].begin();itr!=adjListCompMoves[from].end();itr++) {
+	        if( (*itr).vertex == to) return (*itr).e;
+	    }
+	    return defElem;
+	}
         int size() {
-		    return adjListCompMoves.size();
-		}
+	    return adjListCompMoves.size();
+	}
 };
 
 #endif
